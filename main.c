d3a6070fc6fc0c4a293c6f0e7732653aefcc0a28
Fepas2473 <jrfrancesconi@gmail.com>
2026-02-12 07:57:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -38,30 +39,90 @@ void pisca(int tempo, int vezes, int Pino){
 
     void A(){
    vTaskDelay(250 / portTICK_PERIOD_MS);
-    morse(PONTO,PONTO,PONTO,PONTO);
+    morse(PONTO,LINHA,NADA,NADA);
     printf("A\n"); 
 }
     void B(){
    vTaskDelay(250 / portTICK_PERIOD_MS);
-    morse(PONTO,PONTO,PONTO,PONTO);
+    morse(LINHA,PONTO,PONTO,PONTO);
     printf("B\n"); 
 }
     void C(){
    vTaskDelay(250 / portTICK_PERIOD_MS);
-    morse(PONTO,PONTO,PONTO,PONTO);
+    morse(LINHA,PONTO,LINHA,PONTO);
     printf("C\n"); 
 }
 
     void D(){
    vTaskDelay(250 / portTICK_PERIOD_MS);
-    morse(PONTO,PONTO,PONTO,PONTO);
+    morse(LINHA,PONTO,PONTO,NADA);
     printf("D\n"); 
 }
 
     void E(){
    vTaskDelay(250 / portTICK_PERIOD_MS);
-    morse(LINHA,LINHA,NADA,NADA);
+    morse(PONTO,NADA,NADA,NADA);
     printf("E\n"); 
+}
+
+    void F(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(PONTO,PONTO,LINHA,PONTO);
+    printf("F\n"); 
+}
+
+    void G(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(LINHA,LINHA,PONTO,NADA);
+    printf("G\n"); 
+}
+
+    void H(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(PONTO,PONTO,PONTO,PONTO);
+    printf("H\n"); 
+}
+
+    void I(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(PONTO,PONTO,NADA,NADA);
+    printf("I\n"); 
+}
+
+    void J(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(PONTO,LINHA,LINHA,LINHA);
+    printf("J\n"); 
+}
+
+    void K(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(LINHA,PONTO,LINHA,NADA);
+    printf("K\n"); 
+}
+
+    void L(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(PONTO,LINHA,PONTO,PONTO);
+    printf("L\n"); 
+}
+
+    void M(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(LINHA,LINHA,NADA,NADA);
+    printf("M\n"); 
+}
+
+    void N(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(LINHA,PONTO,NADA,NADA);
+    printf("N\n"); 
+}
+
+    void O(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(LINHA,LINHA,LINHA,NADA);
+    printf("O\n"); 
 }
 
     void P(){
@@ -70,6 +131,65 @@ void pisca(int tempo, int vezes, int Pino){
     printf("P\n"); 
 }
 
+    void Q(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(LINHA,LINHA,PONTO,LINHA);
+    printf("Q\n"); 
+}
+
+    void R(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(PONTO,LINHA,PONTO,NADA);
+    printf("R\n"); 
+}
+
+    void S(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(PONTO,PONTO,PONTO,NADA);
+    printf("S\n"); 
+}
+
+    void T(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(LINHA,NADA,NADA,NADA);
+    printf("T\n"); 
+}
+
+    void U(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(PONTO,PONTO,LINHA,NADA);
+    printf("U\n"); 
+}
+
+    void V(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(PONTO,PONTO,PONTO,LINHA);
+    printf("V\n"); 
+}
+
+    void W(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(PONTO,LINHA,LINHA,NADA);
+    printf("W\n"); 
+}
+
+    void X(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(LINHA,PONTO,PONTO,LINHA);
+    printf("X\n"); 
+}
+
+    void Y(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(LINHA,PONTO,LINHA,LINHA);
+    printf("Y\n"); 
+}
+
+    void Z(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    morse(LINHA,LINHA,PONTO,PONTO);
+    printf("Z\n"); 
+}
 void app_main() {
 
   while (true) {
